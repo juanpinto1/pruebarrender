802a1ec41a5ec67de6417bbf26cd05b6b2975862
juanpinto1 <juan.pinto@cajalosandes.cl>
2024-07-08 21:03:38 -0400

    cambios

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./src/App";
 import { BrowserRouter } from "react-router-dom";
-import { MarketplaceProvider } from "./components/utils/MarketplaceProvider"; // Asegúrate de importar correctamente
+import { MarketplaceProvider } from "./src/components/utils/MarketplaceProvider"; // Asegúrate de importar correctamente
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
